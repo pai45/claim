192bf0d4a2619ad2921558ddf760aee033f4db21
pai45 <priyangshudas@2445@gmail.com>
2026-08-12 01:43:26 +0530

    Fix manage tokens static export

--- app/manage-tokens/[tokenId]/page.tsx
+++ app/manage-tokens/[tokenId]/page.tsx
@@ -1,5 +1,17 @@
 import { TokenDetails } from "@/components/manage-tokens/TokenDetails";
 
+const TOKEN_IDS = [
+  "apple-pay-1",
+  "google-pay-1",
+  "amazon-1",
+  "netflix-1",
+  "spotify-1",
+] as const;
+
+export function generateStaticParams() {
+  return TOKEN_IDS.map((tokenId) => ({ tokenId }));
+}
+
 export default async function TokenDetailsPage({ params }: { params: Promise<{ tokenId: string }> }) {
   const { tokenId } = await params;
   return (
